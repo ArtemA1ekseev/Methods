b45ed634c3ed35375ad77ff2a5af52944ede9b73
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-10-01 21:23:52 +0400

    Методы. Задание 1 - 3.

---
+++ src/homework/Tasks.java
@@ -0,0 +1,53 @@
+package homework;
+
+public class Tasks {
+    public static void main(String[] args) {
+        System.out.println("Задача 1.");
+        int year = 2000;
+        getLeapYear(year);
+        System.out.println(" ");
+        System.out.println("Задача 2.");
+        int clientOS = 0; // (0 — iOS, 1 — Android)
+        int clientDeviceYear = 2015;
+        getClientOS(clientOS, clientDeviceYear);
+        System.out.println(" ");
+        System.out.println("Задача 3.");
+        int DeliveryDistance = 95;
+        getDeliveryDays(DeliveryDistance);
+    }
+    // 1 метод.
+    public static void getLeapYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " — високосный год");
+        } else {
+            System.out.println(year + " — не високосный год");
+        }
+    }
+    // 2 метод.
+    public static void getClientOS(int clientOS, int clientDeviceYear) {
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }
+    }
+    // 3 метод.
+    public static void getDeliveryDays(int DeliveryDistance) {
+        if (DeliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else {
+            int AmountDays = (DeliveryDistance - 20);
+            if ((AmountDays % 40 == 0)) {
+                int NewAmountDays = (AmountDays / 40) + 1;
+                System.out.println("Потребуется дней: " + NewAmountDays);
+            } else {
+                int NewAmountDays2 = (AmountDays / 40) + 2;
+                System.out.println("Потребуется дней: " + NewAmountDays2);
+            }
+        }
+    }
+}
